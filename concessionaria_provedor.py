febe562829ab0fd0a43ec6e6670faffd0b8aedf3
S1mp3r <99444306+S1mp3r@users.noreply.github.com>
2023-12-05 21:20:59 -0300

    Add files via upload
    Atualização da conceissionaria

--- concessionaria_provedor.py
+++ concessionaria_provedor.py
@@ -1,1 +1,16 @@
-#Removido para futuras mudanças
+from flask import Flask, jsonify
+from flask import request
+
+app = Flask(__name__)
+
+#Criado por  Rafael
+carros = [{"modelo": "Carro1", "preco": 20000}, {"modelo": "Carro2", "preco": 25000}]
+vendas = []
+
+@app.route('/carros', methods=['GET'])
+def obter_carros():
+    return jsonify({"carros": carros})
+
+@app.route('/vendas', methods=['GET'])
+def obter_vendas():
+    return jsonify({"vendas": vendas})
