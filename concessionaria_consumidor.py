20d09d2f14bbbed6f9f09926d8e2dc3bb9696015
Vinishows <104833002+Vinishows@users.noreply.github.com>
2023-12-05 21:41:36 -0300

    Update concessionaria_consumidor.py
    Adicionado a função de vendas dos carros da própria concessionaria

--- concessionaria_consumidor.py
+++ concessionaria_consumidor.py
@@ -10,3 +10,31 @@ def obter_carros():
 def obter_vendas():
     response = requests.get(f'{base_url}/vendas')
     return response.json()["vendas"]
+
+
+#Criado por Vinicius
+def realizar_venda(modelo, quantidade):
+    dados_venda = {"modelo": modelo, "quantidade": quantidade}
+    response = requests.post(f'{base_url}/vender', json=dados_venda)
+
+    if response.status_code == 201:
+        print(response.json()["mensagem"])
+    else:
+        print(f"Erro {response.status_code}: {response.json()['mensagem']}")
+
+def obter_carros():
+    response = requests.get(f'{base_url}/carros')
+    return response.json()["carros"]
+
+def obter_vendas():
+    response = requests.get(f'{base_url}/vendas')
+    return response.json()["vendas"]
+
+def adicionar_carro(modelo, preco):
+    dados_carro = {"modelo": modelo, "preco": preco}
+    response = requests.post(f'{base_url}/adicionar_carro', json=dados_carro)
+
+    if response.status_code == 201:
+        print(response.json()["mensagem"])
+    else:
+        print(f"Erro {response.status_code}: {response.json()['mensagem']}")
