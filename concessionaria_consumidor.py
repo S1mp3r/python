80edc9a9c9ed4f5bab3455762730e62f9e19d25d
rafael.depontes <rafael.depontes@dataprev.gov.br>
2025-06-08 22:57:12 -0300

    fix: arrumado codigo do lado do consumidor, refeita revisao do codigo

--- concessionaria_consumidor.py
+++ concessionaria_consumidor.py
@@ -1,18 +1,20 @@
-import requests # type: ignore
+import requests  # type: ignore
 
-#Criado por Rafael
+# Criado por Rafael
 base_url = 'http://localhost:5000'
+
 
 def obter_carros():
     response = requests.get(f'{base_url}/carros')
     return response.json()["carros"]
+
 
 def obter_vendas():
     response = requests.get(f'{base_url}/vendas')
     return response.json()["vendas"]
 
 
-#Criado por Vinicius
+# Criado por Vinicius
 def realizar_venda(modelo, quantidade):
     dados_venda = {"modelo": modelo, "quantidade": quantidade}
     response = requests.post(f'{base_url}/vender', json=dados_venda)
@@ -22,13 +24,6 @@ def realizar_venda(modelo, quantidade):
     else:
         print(f"Erro {response.status_code}: {response.json()['mensagem']}")
 
-def obter_carros():
-    response = requests.get(f'{base_url}/carros')
-    return response.json()["carros"]
-
-def obter_vendas():
-    response = requests.get(f'{base_url}/vendas')
-    return response.json()["vendas"]
 
 def adicionar_carro(modelo, preco):
     dados_carro = {"modelo": modelo, "preco": preco}
@@ -40,15 +35,16 @@ def adicionar_carro(modelo, preco):
         print(f"Erro {response.status_code}: {response.json()['mensagem']}")
 
 
-#Criado por Lucca
+# Criado por Lucca
 def remover_carro(modelo):
     dados_remocao = {"modelo": modelo}
     response = requests.post(f'{base_url}/remover_carro', json=dados_remocao)
 
-    if response.status_code == 200: 
+    if response.status_code == 200:
         print(response.json()["mensagem"])
     else:
         print(f"Erro {response.status_code}: {response.json()['mensagem']}")
+
 
 if __name__ == '__main__':
     carros = obter_carros()
@@ -56,14 +52,18 @@ if __name__ == '__main__':
 
     print("Carros disponíveis:")
     for carro in carros:
-        print(f"Modelo: {carro['modelo']}, Preço: {carro['preco']}")
+        print(f"Modelo: {carro['modelo']}        Preço: {carro['preco']}")
 
     print("\nVendas realizadas:")
     for venda in vendas:
-        print(f"Modelo: {venda['modelo']}, Quantidade: {venda['quantidade']}, Valor: {venda['valor']}")
+        print(
+            f"Modelo: {venda['modelo']}        Quantidade: {venda['quantidade']}          Valor: {venda['valor']}")
 
-# Adicionar um novo carro
-adicionar_carro("Carro3", 30000)
+    # Adicionar um novo carro
+    adicionar_carro("Porshe", 3000000)
 
-# Remover um carro do estoque
-remover_carro("Carro2")
+    # Faz a venda de um carro do estoque
+    realizar_venda("Chevrolet", 200000)
+
+    # Remover um carro do estoque
+    remover_carro("Chevrolet")
